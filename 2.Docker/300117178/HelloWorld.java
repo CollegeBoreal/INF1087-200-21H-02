5d634d246adba9c86cfe8194b4422599a8329ed1
willfrid boris <boriskwayep35@yahoo.com>
2021-02-04 17:29:01 -0500

    :boris:

--- 2.Docker/300117178/HelloWorld.java
+++ 2.Docker/300117178/HelloWorld.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
  class HelloWorld{
 
 	public static void main(String[] args){
+		 Scanner sc = new Scanner(System.in); 
+		 System.out.println("#######################");
+		 System.out.println("HelloWorld TO Boris Machine Java");
 
-		System.out.println("HelloWorld From Boris");
+		 System.out.println("#######################");
+		 System.out.println("What your name Dear Developer ? :\n");
+                 String name = sc.nextLine(); 
+
+		 System.out.println("#######################");
+                 System.out.println("Welcome Developer "+name); 
 
 }
 
