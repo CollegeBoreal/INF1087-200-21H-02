ff3f38d40ed7f8222ead51bb13e0f84a08751b6a
b300098957 <b300098957@collegeboreal.ca>
2021-04-02 12:37:38 -0400

    .

--- 5.Metal/.scripts/participation.sh
+++ 5.Metal/.scripts/participation.sh
@@ -33,7 +33,11 @@ echo ""
 echo "|:hash:| Boréal :id:                | Interne            | ssh | Docker Engine | LVG |"
 echo "|------|----------------------------|--------------------|-----|---------------|-----|"
 
+NOSSH=" :x: | :x: | :x |"
+
 i=0
+OK=":heavy_check_mark:"
+KO=":x:"
 
 for id in "${ETUDIANTS[@]}"
 do
@@ -42,30 +46,37 @@ do
         -o PasswordAuthentication=no \
         -o ConnectTimeout=5 ${SERVERS[${i}]} lsb_release -a 2>/dev/null`
    # echo $VERSION
+
    DOCKER=`ssh -i ~/.ssh/b300098957@ramena.pk \
         -o StrictHostKeyChecking=no \
         -o PasswordAuthentication=no \
         -o ConnectTimeout=5 ${SERVERS[${i}]} systemctl status docker 2>/dev/null`
+   # echo $DOCKER
+
    LVG=`ssh -i ~/.ssh/b300098957@ramena.pk \
         -o StrictHostKeyChecking=no \
         -o PasswordAuthentication=no \
         -o ConnectTimeout=5 ${SERVERS[${i}]} sudo lvs ubuntu-vg/iscsi-lv --noheadings 2>/dev/null`
-   OKII="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | :tada: | :100:g |"
-   OKI="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | :tada: | :x: |"
-   OK="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | :x: | :x: |"
-   KO="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :x: | :x: | :x: |"
-   if [[ $VERSION == *"Ubuntu"* && $DOCKER == *"(running)"* && $LVG == *"-wi-a-----"* ]]; then
-       echo ${OKII}
-   else
-       if [[ $VERSION == *"Ubuntu"* && $DOCKER == *"(running)"* ]]; then
-           echo ${OKI}
+   # echo $LVG
+
+   VALUE="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` |"
+
+   if [[ $VERSION == *"Ubuntu"* ]]; then
+       VALUE="${VALUE} ${OK} |"
+       if [[ $DOCKER == *"(running)"* ]]; then
+          VALUE="${VALUE} ${OK} |"
        else
-           if [[ $VERSION == *"Ubuntu"* ]]; then
-               echo ${OK}
-           else
-               echo ${KO}
-           fi
+          VALUE="${VALUE} ${KO} |"
        fi
+       if [[ $LVG == *"-wi-a-----"* ]]; then
+          VALUE="${VALUE} ${OK} |"
+       else
+          VALUE="${VALUE} ${KO} |"
+       fi
+   else
+       VALUE="${VALUE} ${KO} | ${NOSSH}"
    fi
+   echo ${VALUE}
    let "i++"
+
 done
