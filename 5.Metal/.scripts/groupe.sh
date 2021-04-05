622d4b893afea75603ffbe1e887ed5e1bc839f6f
b300098957 <b300098957@collegeboreal.ca>
2021-04-05 14:10:28 -0400

    .

---
+++ 5.Metal/.scripts/groupe.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# --------------------------------------
+# Fonctions 
+#
+#
+# --------------------------------------
+PERIPHERIQUES=(
+":zero:"
+":one:"
+":two:"
+":three:"
+)
+
+GROUPE="boreal"
+
+# --------------------------------------
+#
+#
+#
+# --------------------------------------
+
+source .scripts/grappes.sh --source-only
+   
+echo "# Participation du groupe au `date +"%d-%m-%Y %H:%M"`"
+echo ""
+
+
+echo "| Table des matières            | Description                                             |"
+echo "|-------------------------------|---------------------------------------------------------|"
+echo "| :a: [Présence](#a-présence)   | Le groupe a fait le travail        :white_check_mark:   |"
+echo "| :b: [Précision](#b-précision) | Le groupe a réussi son travail     :tada:               |"
+
+echo ""
+echo "## Légende"
+echo ""
+
+echo "| Signe              | Signification                 |"
+echo "|--------------------|-------------------------------|"
+echo "| :white_check_mark: | Prêt à être corrigé           |"
+echo "| :x:                | Étape inexistante             |"
+
+echo ""
+echo "## :a: Présence"
+echo ""
+echo "|:hash:| Grappe :wheel_of_dharma: | :gear: Config | :roll_of_paper: BlockDevice |"
+echo "|------|--------------------------|---------------|-----------------------------|"
+
+i=0
+OK=":white_check_mark:"
+KO=":x:"
+
+for grappe in "${GRAPPES[@]}"
+do
+
+   FOLDER=${DIR[${i}]}.${grappe}
+
+   VALUE="| ${i} | [${FOLDER}](${FOLDER}) |"
+
+   echo ${VALUE}
+   let "i++"
+
+done
